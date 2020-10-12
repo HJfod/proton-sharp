b00b6dc4b71f0de920309294fe94ff03682662f2
hjfod <dreadrollmusic@gmail.com>
2020-10-12 13:51:41 +0300

    textarea remembers tabs and spaces

--- elements/Textarea.cs
+++ elements/Textarea.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace proton {
@@ -8,6 +10,18 @@ namespace proton {
                 this.BorderStyle = BorderStyle.None;
                 this.AcceptsTab = true;
                 this.DoubleBuffered = true;
+
+                this.KeyDown += (s, e) => {
+                    if (e.KeyCode == Keys.Enter) {
+                        try {
+                            string curr = this.Lines[this.GetLineFromCharIndex(this.SelectionStart)];
+                            this.SelectedText = "\n"
+                            + new String('\t', curr.Count(f => f == '\t'))
+                            + new String(' ',  curr.TakeWhile(c => c == ' ').Count());
+                            e.Handled = true;
+                        } catch (Exception err) {};
+                    }
+                };
             }
         }
     }
